4e5f39e97321f97dee75e05829572cd2511c943a
Joshua Martinez <jam@Joshuas-MacBook-Pro.local>
2019-09-11 22:54:41 -0500

    class work

--- JS 411/fetch-app/src/components/Beer.js
+++ JS 411/fetch-app/src/components/Beer.js
@@ -1,10 +1,13 @@
-
-
-class Beer extends Component{
-        render(){
-            return(
+import React, { Component } from 'react'
+class Beer extends Component  {
+    render(){
+        return (
+            <div>
                 <p>{this.props.beer.id}</p>
                 <p>{this.props.beer.name}</p>
-            )
-        }
+            </div>
+        )
+    }
 }
+
+export default Beer
--- JS 411/fetch-app/src/components/Main.js
+++ JS 411/fetch-app/src/components/Main.js
@@ -1,4 +1,34 @@
-import React,  { Component } from 'react'
+import React, { Component } from 'react'
 import Beer from './Beer'
 
-class Main extends 
+class Main extends Component {
+    state = {
+        beers: []
+    }
+
+    componentDidMount() {
+        fetch ('https://api.punkapi.com/v2/beers')
+            .then(res => res.json())
+            .then(parsedJSON => console.log(parsedJSON))
+            .then(data =>  {
+                this.setState({beers: data})
+            })
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>The Beer List:</h2>
+                {
+                    this.state.beers.map(b => {
+                        return (
+                            <Beer key={b.id} beer={b}/>
+                        )
+                    })
+                }
+            </div>
+        )
+    }
+}
+
+export default Main
