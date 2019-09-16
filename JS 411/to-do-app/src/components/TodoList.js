784e525f8807777523a5098286bcada53042ae14
Joshua Martinez <jam@Joshuas-MBP.austin.rr.com>
2019-09-15 20:56:36 -0500

    sunday push

---
+++ JS 411/to-do-app/src/components/TodoList.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import TodoForm from './TodoForm';
+
+
+export default class TodoList extends React.Component {
+    state ={
+        todos: []
+    };
+
+    addTodo = (todo) => {
+        this.setState({
+            todos: [todo, ...this,state.todos]
+        })
+    }
+
+    render() {
+        return(
+            <div>
+                <TodoForm onSubmit={this.addTodo}/>
+                {this.state.todos.map(todo => (
+                    <div key={todo.id}>{todo.text}</div>
+                ))}
+            </div>
+        );
+    }
+}
+//
